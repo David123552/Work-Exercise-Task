fe7cb764473e54a47a4791a15af32e304e71f0be
David123552 <153772105+David123552@users.noreply.github.com>
2023-12-14 21:06:36 +0000

    Corrected place

--- WebForm1.aspx
+++ WebForm1.aspx
@@ -8,59 +8,7 @@
 <Script language="C#" runat="server">
 
 
-    async void postcodebutton_Click(Object sender, EventArgs e)
-    {
-        string inputValue = postcodebox.Text.Trim(); // Get text from the TextBox
-        if (!string.IsNullOrEmpty(inputValue))
-        {
-            
-            string apiUrl = "https://api.getAddress.io/autocomplete";
-
-            using (HttpClient client = new HttpClient())
-            {
-                try
-                {
-                    // Make a request to the external API
-                    HttpResponse response = await client.GetAsync(apiUrl + "?input=" + inputValue);
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        // Read the response content
-                        string responseData = await response.Content.ReadAsStringAsync();
-                     
-                        //Processes the response data. I know that it isnt comma-seperated, it is just a placeholder.
-                        string[] options = responseData.Split(',');
-
-                        // Populate the DropDownList with the received options
-                        ddlOptions.Items.Clear();
-                        foreach (string option in options)
-                        {
-                            ddlOptions.Items.Add(option);
-                        }
-                    }
-                    else
-                    {
-                        //Error Handling. If it fails, itll just say that it fails
-                        ddlOptions.Items.Clear();
-                        ddlOptions.Items.Add("API request failed");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    //Supposedly handles exceptions such as network errors, timeouts, etc.
-                    ddlOptions.Items.Clear();
-                    ddlOptions.Items.Add("Error: " + ex.Message);
-                }
-            }
-        }
-        else
-        {
-            // Handle the case where the TextBox is empty
-            ddlOptions.Items.Clear();
-            ddlOptions.Items.Add("Please enter a value");
-        }
-
-    }
+    
 </Script>
 
 
--- WebForm1.aspx.cs
+++ WebForm1.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -13,5 +14,60 @@ namespace MyWebApp
         {
 
         }
+
+        protected void postcodebutton_Click(Object sender, EventArgs e)
+        {
+            string inputValue = postcodebox.Text.Trim(); // Get text from the TextBox
+            if (!string.IsNullOrEmpty(inputValue))
+            {
+
+                string apiUrl = "https://api.getAddress.io/autocomplete";
+
+                using (HttpClient client = new HttpClient())
+                {
+                    try
+                    {
+                        // Make a request to the external API
+                        HttpResponseMessage response = client.GetAsync(apiUrl + "?input=" + inputValue).Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            // Read the response content
+                            string responseData = response.Content.ReadAsStringAsync().Result;
+
+                            //Processes the response data. I know that it isnt comma-seperated, it is just a placeholder.
+                            string[] options = responseData.Split(',');
+
+                            // Populate the DropDownList with the received options
+                            ddlOptions.Items.Clear();
+                            foreach (string option in options)
+                            {
+                                ddlOptions.Items.Add(option);
+                            }
+                        }
+                        else
+                        {
+                            //Error Handling. If it fails, itll just say that it fails
+                            ddlOptions.Items.Clear();
+                            ddlOptions.Items.Add("API request failed");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //Supposedly handles exceptions such as network errors, timeouts, etc.
+                        ddlOptions.Items.Clear();
+                        ddlOptions.Items.Add("Error: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                // Handle the case where the TextBox is empty
+                ddlOptions.Items.Clear();
+                ddlOptions.Items.Add("Please enter a value");
+            }
+
+        }
+
     }
 }
